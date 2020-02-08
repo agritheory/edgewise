4273c44a7945a3b7982d808e600ade68d742cb78
Tyler Matteson <tyler@agritheory.com>
2020-02-07 19:41:46 -0500

    chore: clean up example

--- examples/example.py
+++ examples/example.py
@@ -7,20 +7,11 @@ import edgedb
 
 def connect_to_example_db():
     dotenv.load_dotenv()
-    print(
-        os.getenv("EDGEDB_HOST"),
-        os.getenv("EDGEDB_USER"),
-        os.getenv("EDGEDB_PASSWORD"),
-        os.getenv("EDGEDB_DATABASE"),
-    )
     return edgedb.connect(
         host=os.getenv("EDGEDB_HOST", default="localhost"),
-        # port=os.getenv('EDGEDB_PORT', default=5656),
-        # admin=os.getenv('EDGEDB_ADMIN', default=False),
         user=os.getenv("EDGEDB_USER", default="edgedb"),
         password=os.getenv("EDGEDB_PASSWORD"),
         database=os.getenv("EDGEDB_DATABASE", default="example"),
-        # timeout=os.getenv('EDGEDB_TIMEOUT', default=60),
     )
 
 
