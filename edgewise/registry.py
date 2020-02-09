2c5ed227202ef9394e781a73042058565a3d2dcc
Tyler Matteson <tyler@agritheory.com>
2020-02-08 22:05:22 -0500

    fix: hide module from repr by default

--- edgewise/registry.py
+++ edgewise/registry.py
@@ -14,13 +14,14 @@ from .edb_utils import type_map
 class ClassRegistry:
     registry = attrib(default={})
     database = attrib(default=None)
+    repr_timestamps = attrib(default=False)
 
     def __attrs_post_init__(self) -> typing.NoReturn:
         classes = self.inspect_db()
         if classes:
             self.build_classes(classes)
 
-    # def mutate(self, key, value):
+    # def mutate(self, key, value):  # better name update?
     #     return attr.evolve(self, key=value)
 
     def register(self, key: str, cls: type) -> typing.NoReturn:
@@ -61,12 +62,17 @@ class ClassRegistry:
     def _build_attributes(self, obj: edgedb.Object) -> dict:
         object_module, object_name = obj.name.split("::")
         attributes = {}
-        attributes["__edbmodule__"] = attrib(default=object_module, type=str)
+        attributes["__edbmodule__"] = attrib(default=object_module, type=str, repr=False)
         for prop in obj.properties:
             prop_type = type_map.get(prop.target.name.split("::")[1])
             if prop.name in ("id"):
                 continue
+            # if prop.name in ("id", "__edbmodule__"):
+            #     attributes[prop.name] = attrib(default=None, type=prop_type, repr=self.repr_id_and_module)
+            # if prop.name in ("__createdutc__", "__modifiedutc__"):
+            #     attributes[prop.name] = attrib(default=None, type=prop_type, repr=self.repr_timestamps)
             attributes[prop.name] = attrib(default=None, type=prop_type)
+
             # need a repr=**obfuscated** for password scalar
             # password = attrib(repr=lambda value: '***')
         for link in obj.links:
