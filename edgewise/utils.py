a816fb47a328d4679810660a19a2d866bde8851e
Tyler Matteson <tyler@agritheory.com>
2020-02-09 15:50:33 -0500

    chore: move database connection functions to utils

--- edgewise/utils.py
+++ edgewise/utils.py
@@ -1,5 +1,27 @@
 import edgedb
+import dotenv
+import os
+import typing
 
+
+def connect_sync(connection: typing.Optional[edgedb.BlockingIOConnection] = None) -> edgedb.BlockingIOConnection:
+    dotenv.load_dotenv()
+    return edgedb.connect(
+        host=os.getenv("EDGEDB_HOST", default="localhost"),
+        user=os.getenv("EDGEDB_USER", default="edgedb"),
+        password=os.getenv("EDGEDB_PASSWORD"),
+        database=os.getenv("EDGEDB_DATABASE", default="example"),
+    )
+
+
+def connect_async():
+    dotenv.load_dotenv()
+    return edgedb.async_connect(
+        host=os.getenv("EDGEDB_HOST", default="localhost"),
+        user=os.getenv("EDGEDB_USER", default="edgedb"),
+        password=os.getenv("EDGEDB_PASSWORD"),
+        database=os.getenv("EDGEDB_DATABASE", default="example"),
+    )
 
 def load_edgeql_file(path) -> str:
     pass
