03142612343bbae296cbfe1c5c4365de26f1938f
Tyler Matteson <tyler@agritheory.com>
2020-02-15 21:01:56 -0500

    fix: reorder and black imports

--- edgewise/__init__.py
+++ edgewise/__init__.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 
-__version__ = ""
-
-# import edgewise
+from .connections import EdgeDBConnection
 from .edgewise import (
+    CustomScalar,
+    DefaultEnum,
+    Document,
+    class_registry,
     connect,
     get_doc,
     new_doc,
-    register,
-    register_with_schema,
     new_scalar,
+    register,
     register_scalar,
     register_scalar_with_schema,
-    Document,
-    CustomScalar,
-    DefaultEnum,
+    register_with_schema,
 )
+from .registry import ClassRegistry
+
+__version__ = "0.1.0"
