acfec5dc1b4f8f63039715a217b6976b8d5d22b4
Tyler Matteson <tyler@agritheory.com>
2020-03-22 16:25:24 -0400

    feat: update create data (lots of async errors), but its mostly working

--- tests/create_data.py
+++ tests/create_data.py
@@ -1,7 +1,14 @@
+
+import asyncio
+import random
+
 import edgedb
 import mimesis
+import pytest
 
 import edgewise
+from .conftest import get_connection_object
+
 
 user_schema = """
 CREATE MIGRATION init_user TO {
@@ -30,42 +37,66 @@ COMMIT MIGRATION init_user;
 """
 
 
-def create_records(company=1, user=1, rbac_role=1):
+def build_schema():
+    connection_object = get_connection_object()
+    conn = connection_object('sync')
+    with conn.transaction():
+        conn.execute(user_schema)
+
+
+def create_registry():
+    connection_object = get_connection_object()
+    edgewise.class_registry = edgewise.ClassRegistry(connection_object)
+
+
+def create_records(company=2, user=10, rbac_role=4, save=False):
+    create_registry()
+    asyncio.run(_create_records(company, user, rbac_role, save))
+
+
+async def _create_records(company, user, rbac_role, save):
+    companies, rbac_roles = [], []
     for i in range(0, company):
-        create_company()
-    for i in range(0, user):
-        create_user()
+        companies.append(await create_company(save))
     for i in range(0, rbac_role):
-        create_rbac_role()
+        rbac_roles.append(await create_rbac_role(save))
+    for i in range(0, user):
+        company = random.choice(companies).name
+        role = random.choice(rbac_roles).name
+        await create_user(company, role, save)
 
 
-def create_company(save=False):
+async def create_company(save):
     company = edgewise.new_doc("Company")
     company.name = mimesis.Person("en").last_name() + " "\
         + mimesis.Business().company_type(abbr=True)
     company.country = "United States"
+    print(company.name)
     if save:
-        company.save()
+        await company.save()
     return company
 
 
-def create_user(save=False):
+async def create_user(company, role, save):
     person = mimesis.Person("en")
     user = edgewise.new_doc("User")
     user.first_name = person.name()
     user.last_name = person.last_name()
     user.email = person.email()
-    user.password = person.password()
+    user.password = 'password'
     user.username = person.username()
-    user.rbac_role = edgewise.new_doc("RBACRole", name='Database Administrator')
+    # user.rbac_role = role
+    # user.company = company
+    print(user.username)
     if save:
-        user.save()
+        await user.save()
     return user
 
 
-def create_rbac_role(save=False):
+async def create_rbac_role(save):
     role = edgewise.new_doc("RBACRole")
     role.name = mimesis.Person("en").occupation().replace(' ', '')
+    print(role.name)
     if save:
-        role.save()
+        await role.save()
     return role
