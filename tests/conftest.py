7c25d7b391127e7a7d0d3605372a48ac50f5175e
Tyler Matteson <tyler@agritheory.com>
2020-04-01 17:40:26 -0400

    tests: create data add schema and base data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+import asyncio
+import tracemalloc
+
 import pytest
 
 import edgewise
+from tests.create_data import user_schema, create_company, create_user, create_rbac_role
 
+tracemalloc.start()
 
 def get_connection_object():
     return edgewise.EdgeDBConnection(
@@ -18,12 +23,20 @@ def get_connection_object():
 
 def pytest_configure(config):
     connection_object = get_connection_object()
+    conn = connection_object("sync")
+    with conn.transaction():
+        conn.execute(user_schema)
+        print("Imported Schema")
     edgewise.class_registry = edgewise.ClassRegistry(connection_object)
+    asyncio.run(create_company())
+    asyncio.run(create_rbac_role())
+    asyncio.run(create_user())
     print("Registered Classes:")
     [
         print("{}: {}".format(key, value))
         for key, value in edgewise.class_registry.registry.items()
     ]
+
 
 
 @pytest.fixture(scope="module")
--- tests/create_data.py
+++ tests/create_data.py
@@ -1,12 +1,10 @@
 import asyncio
-import random
+from pathlib import Path
+import csv
 
 import edgedb
-import mimesis
-import pytest
 
 import edgewise
-from .conftest import get_connection_object
 
 
 user_schema = """
@@ -36,69 +34,36 @@ COMMIT MIGRATION init_user;
 """
 
 
-def build_schema():
-    connection_object = get_connection_object()
-    conn = connection_object("sync")
-    with conn.transaction():
-        conn.execute(user_schema)
+async def create_company():
+    file_path = Path(__file__).parent / 'company.csv'
+    with open(file_path) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in csvreader:
+            company = edgewise.new_doc("Company")
+            company.name = row[0]
+            company.country = row[1]
+            await company.save()
 
 
-def create_registry():
-    connection_object = get_connection_object()
-    edgewise.class_registry = edgewise.ClassRegistry(connection_object)
+async def create_user():
+    file_path = Path(__file__).parent / 'user.csv'
+    with open(file_path) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in csvreader:
+            print(row[0])
+            user = edgewise.new_doc("User")
+            user.first_name = row[0]
+            user.last_name = row[1]
+            user.email = row[2]
+            user.password = '<raw_str>' + row[3]
+            user.username = user.email
+            user.company = await edgewise.get_doc('Company', {'name': row[4]})
+            user.rbac_role = await edgewise.get_doc("RBACRole", {'name': row[5]})
+            await user.save()
 
 
-def create_records(company=2, user=10, rbac_role=4, save=False):
-    create_registry()
-    asyncio.run(_create_records(company, user, rbac_role, save))
-
-
-async def _create_records(company, user, rbac_role, save):
-    companies, rbac_roles = [], []
-    for i in range(0, company):
-        companies.append(await create_company(save))
-    for i in range(0, rbac_role):
-        rbac_roles.append(await create_rbac_role(save))
-    for i in range(0, user):
-        company = random.choice(companies).name
-        role = random.choice(rbac_roles).name
-        await create_user(company, role, save)
-
-
-async def create_company(save):
-    company = edgewise.new_doc("Company")
-    company.name = (
-        mimesis.Person("en").last_name()
-        + " "
-        + mimesis.Business().company_type(abbr=True)
-    )
-    company.country = "United States"
-    print(company.name)
-    if save:
-        await company.save()
-    return company
-
-
-async def create_user(company, role, save):
-    person = mimesis.Person("en")
-    user = edgewise.new_doc("User")
-    user.first_name = person.name()
-    user.last_name = person.last_name()
-    user.email = person.email()
-    user.password = "password"
-    user.username = person.username()
-    # user.rbac_role = role
-    # user.company = company
-    print(user.username)
-    if save:
-        await user.save()
-    return user
-
-
-async def create_rbac_role(save):
-    role = edgewise.new_doc("RBACRole")
-    role.name = mimesis.Person("en").occupation().replace(" ", "")
-    print(role.name)
-    if save:
+async def create_rbac_role():
+    for role_name in ('Base User', 'Guest', 'Administrator'):
+        role = edgewise.new_doc("RBACRole")
+        role.name = role_name
         await role.save()
-    return role
